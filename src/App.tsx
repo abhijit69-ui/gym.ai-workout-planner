2e6d3e0a0b73f0aa592c6abd416218fc6546df45
Abhijit Nath <abhijitnath1a1@gmail.com>
2026-03-06 12:27:27 +0530

    Navbar & Auth Context

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,31 @@ import Onboarding from './pages/Onboarding';
 import ProfilePage from './pages/ProfilePage';
 import AuthPage from './pages/AuthPage';
 import AccountPage from './pages/AccountPage';
+import Navbar from './components/layout/Navbar';
+import { NeonAuthUIProvider } from '@neondatabase/neon-js/auth/react';
+import { authClient } from './lib/auth';
+import AuthProvider from './context/AuthContext';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<HomePage />} />
-        <Route path='/onboarding' element={<Onboarding />} />
-        <Route path='/profile' element={<ProfilePage />} />
-        <Route path='/auth/:pathname' element={<AuthPage />} />
-        <Route path='/auth/:pathname' element={<AccountPage />} />
-      </Routes>
-    </BrowserRouter>
+    <NeonAuthUIProvider authClient={authClient} defaultTheme='dark'>
+      <AuthProvider>
+        <BrowserRouter>
+          <div className='min-h-screen flex flex-col'>
+            <Navbar />
+            <main className='flex-1'>
+              <Routes>
+                <Route index element={<HomePage />} />
+                <Route path='/onboarding' element={<Onboarding />} />
+                <Route path='/profile' element={<ProfilePage />} />
+                <Route path='/auth/:pathname' element={<AuthPage />} />
+                <Route path='/auth/:pathname' element={<AccountPage />} />
+              </Routes>
+            </main>
+          </div>
+        </BrowserRouter>
+      </AuthProvider>
+    </NeonAuthUIProvider>
   );
 };
 
--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,3 +1,13 @@
+import { AuthView } from '@neondatabase/neon-js/auth/react';
+import { useParams } from 'react-router-dom';
+
 export default function AuthPage() {
-  return <div>AuthPage</div>;
+  const { pathname } = useParams();
+  return (
+    <div className='min-h-screen pt-24 px-6 flex items-center justify-center '>
+      <div className='max-w-md w-full'>
+        <AuthView path={pathname} />
+      </div>
+    </div>
+  );
 }
